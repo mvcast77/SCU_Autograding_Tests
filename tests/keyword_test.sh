b2895a1310f4c3792b7f5a51a7e26b065e458bce
Michael Castillo <pluto@Michaels-MacBook-Pro.local>
2025-11-15 21:18:15 -0800

    Test capture group with sed
    Trying to use capture groups to print out the line instead of
    string substitution

--- tests/keyword_test.sh
+++ tests/keyword_test.sh
@@ -5,3 +5,6 @@ sed -n '/MAX_LENGTH/p' $TARGET
 
 echo "Lines with first pattern, without second pattern"
 sed -n '/MAX_LENGTH/{/#define/!p}' $TARGET
+
+echo "Trying to grab the name of the macro"
+sed -nr '/#define[\s\t]+\(.\)$/\1/p' $TARGET
