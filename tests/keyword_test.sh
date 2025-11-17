c69a714b6915fc25c6e7b080dea41e4bb20713c4
Michael Castillo <pluto@Michaels-MacBook-Pro.local>
2025-11-16 16:47:57 -0800

    Proper form of grabbing the macro name
    Proper sed expression, that substitutes the matched line with the
    capture group in order to get the name of the macro. We will check
    for proper usage of this macro soon.

--- tests/keyword_test.sh
+++ tests/keyword_test.sh
@@ -7,4 +7,5 @@ echo "Lines with first pattern, without second pattern"
 sed -n '/MAX_LENGTH/{/#define/!p}' $TARGET
 
 echo "Trying to grab the name of the macro"
-sed -nr '/#define[\s\t]+\(.\)$/\1/p' $TARGET
+output=$(sed -nE 's/^#define[ \s\t]+([^ \s\t]).*$/\1/p' $TARGET)
+echo $output
