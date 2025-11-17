6edff13848de39f3afb2da6e202a7f7427475e6e
Michael Castillo <pluto@Michaels-MacBook-Pro.local>
2025-11-16 16:54:29 -0800

    Full testing of macro usage
    This form could be expanded to check for specific variable usage,
    or for particular expression patterns.
    Ideally, the final sed expression will match lines with the output
    variable in it, but not the defining statement, and print said lines.

--- tests/keyword_test.sh
+++ tests/keyword_test.sh
@@ -7,5 +7,7 @@ echo "Lines with first pattern, without second pattern"
 sed -n '/MAX_LENGTH/{/#define/!p}' $TARGET
 
 echo "Trying to grab the name of the macro"
-output=$(sed -nE 's/^#define[ \s\t]+([^ \s\t]).*$/\1/p' $TARGET)
-echo $output
+output=$(sed -nE 's/^#define[ \s\t]+([^ \s\t]+).*$/\1/p' $TARGET)
+
+echo "Using the macro name to check for future usage"
+sed -nE "/^.*($output).*$/{/#define/!p}" $TARGET
